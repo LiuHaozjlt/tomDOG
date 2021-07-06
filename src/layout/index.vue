c8664da6ac94f9a211c02ae11666e2f208096084
panjinshan <panjinshan@iauto.com>
2021-07-06 20:36:32 +0800

    modifield questions *3

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,47 +1,77 @@
 <template>
   <div :class="classObj" class="app-wrapper">
-    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <div
+      v-if="device === 'mobile' && sidebar.opened"
+      class="drawer-bg"
+      @click="handleClickOutside"
+    />
     <div v-if="!isFull">
       <sidebar class="sidebar-container" />
     </div>
 
-    <div :class="{hasTagsView:needTagsView}" class="main-container" :style="{marginLeft:(isFull?0:'210px')}">
-        <div :class="{'fixed-header':fixedHeader}">
-          <navbar :isShow="isShow" @changeComponentData="handleComponent"  @changeComponentDataCopy="handleComponentTwo"/>
-          <tags-view v-if="needTagsView" />
-        </div>
-        <app-main />
-        <right-panel v-if="showSettings">
-          <settings />
-        </right-panel>
-        <!-- Modal -->
-        <div class="modal" v-show="isShow">
-            <div class="modal-box">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h3>对话框标题</h3>
-              </div>
-              <div class="modal-body">
-                <iframe src="http://192.168.1.21:9527/#/dashboard"  width="460" height="auto" frameborder="no" border="0"></iframe>
-              </div>
-              <div class="modal-footer">
-                <a href="#" class="btn" @click="isShow=false">关闭</a>
-                <a href="#" class="btn btn-primary">Save changes</a>
-              </div>
-            </div>
-        </div>
-        <!-- 弹框 -->
-        <el-dialog
-          title="提示"
-          :visible.sync="dialogVisible">
-          <div><app-main /></div>
-          <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-          </span>
-        </el-dialog>
-
+    <div
+      :class="{ hasTagsView: needTagsView }"
+      class="main-container"
+      :style="{ marginLeft: isFull ? 0 : '210px' }"
+    >
+      <div :class="{ 'fixed-header': fixedHeader }">
+        <navbar
+          :is-show="isShow"
+          @changeComponentData="handleComponent"
+          @changeComponentDataCopy="handleComponentTwo"
+        />
+        <tags-view v-if="needTagsView" />
       </div>
+      <app-main />
+      <right-panel v-if="showSettings">
+        <settings />
+      </right-panel>
+
+      <!-- Modal -->
+      <div
+        v-show="isShow"
+        class="modal"
+      >
+        <div class="modal-box">
+          <div class="modal-header">
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-hidden="true"
+            >
+              &times;
+            </button>
+            <h3>对话框标题</h3>
+          </div>
+          <div class="modal-body">
+            <iframe
+              src="http://192.168.1.21:9527/#/dashboard"
+              width="460"
+              height="auto"
+              frameborder="no"
+              border="0"
+            />
+          </div>
+          <div class="modal-footer">
+            <a href="#" class="btn" @click="isShow = false">关闭</a>
+            <a href="#" class="btn btn-primary">Save changes</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- 弹框 -->
+      <el-dialog title="提示" :visible.sync="dialogVisible">
+        <div><app-main /></div>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button
+            type="primary"
+            @click="dialogVisible = false"
+          >确 定</el-button>
+        </span>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
@@ -53,13 +83,6 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'Layout',
-  data() {
-    return {
-      isShow:false,
-      dialogVisible: false,
-      isFull:false
-    }
-  },
   components: {
     AppMain,
     Navbar,
@@ -69,13 +92,20 @@ export default {
     TagsView
   },
   mixins: [ResizeMixin],
+  data() {
+    return {
+      isShow: false,
+      dialogVisible: false,
+      isFull: false
+    }
+  },
   computed: {
     ...mapState({
-      sidebar: state => state.app.sidebar,
-      device: state => state.app.device,
-      showSettings: state => state.settings.showSettings,
-      needTagsView: state => state.settings.tagsView,
-      fixedHeader: state => state.settings.fixedHeader
+      sidebar: (state) => state.app.sidebar,
+      device: (state) => state.app.device,
+      showSettings: (state) => state.settings.showSettings,
+      needTagsView: (state) => state.settings.tagsView,
+      fixedHeader: (state) => state.settings.fixedHeader
     }),
     classObj() {
       return {
@@ -86,79 +116,78 @@ export default {
       }
     }
   },
+  mounted() {
+    this.$EventBus.$on('updateFull', (val) => {
+      this.isFull = val
+      console.log('我要开始打印接收数据了====》', val)
+    })
+  },
   methods: {
     handleClickOutside() {
       this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
     },
     handleComponent(params) {
-      this.isShow = params;
+      this.isShow = params
     },
-    handleComponentTwo(params){
-      this.dialogVisible = params;
+    handleComponentTwo(params) {
+      this.dialogVisible = params
     }
-  },
-  mounted(){
-    this.$EventBus.$on("updateFull",val=>{
-      this.isFull = val;
-      console.log("我要开始打印接收数据了====》",val)
-    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  @import "~@/styles/mixin.scss";
-  @import "~@/styles/variables.scss";
+@import "~@/styles/mixin.scss";
+@import "~@/styles/variables.scss";
 
-  .app-wrapper {
-    @include clearfix;
-    position: relative;
-    height: 100%;
-    width: 100%;
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
 
-    &.mobile.openSidebar {
-      position: fixed;
-      top: 0;
-    }
-  }
-
-  .drawer-bg {
-    background: #000;
-    opacity: 0.3;
-    width: 100%;
-    top: 0;
-    height: 100%;
-    position: absolute;
-    z-index: 999;
-  }
-
-  .fixed-header {
+  &.mobile.openSidebar {
     position: fixed;
     top: 0;
-    right: 0;
-    z-index: 9;
-    width: calc(100% - #{$sideBarWidth});
-    transition: width 0.28s;
   }
+}
 
-  .hideSidebar .fixed-header {
-    width: calc(100% - 54px)
+.drawer-bg {
+  background: #000;
+  opacity: 0.3;
+  width: 100%;
+  top: 0;
+  height: 100%;
+  position: absolute;
+  z-index: 999;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+  transition: width 0.28s;
+}
+
+.hideSidebar .fixed-header {
+  width: calc(100% - 54px);
+}
+
+.mobile .fixed-header {
+  width: 100%;
+}
+
+.modal {
+  display: block;
+  background: gray;
+  position: absolute; inset: 0px; z-index: 2000;
+  .modal-box {
+    background: #fff;
+    width: 500px;
+    height: 500px;
+    margin: 30px auto;
   }
-
-  .mobile .fixed-header {
-    width: 100%;
-  }
-
-
-  .modal{
-    display: block;
-    background:gray;
-    .modal-box{
-      background: #fff;
-      width: 500px;
-      height: 500px;
-      margin: 30px auto;
-    }
-  }
-
+}
 </style>
--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -4,36 +4,59 @@
 
     <panel-group @handleSetLineChartData="handleSetLineChartData" />
 
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData" v-if="false" />
+    <el-row style="background: #fff; padding: 16px 16px 0; margin-bottom: 32px">
+      <line-chart v-if="false" :chart-data="lineChartData" />
     </el-row>
 
     <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart />
-        </div>
-      </el-col>
+      <VueAgile>
+        <el-col :xs="24" :sm="24" :lg="8">
+          <div class="chart-wrapper">
+            <raddar-chart />
+          </div>
+        </el-col>
+        <el-col :xs="24" :sm="24" :lg="8">
+          <div class="chart-wrapper">
+            <pie-chart />
+          </div>
+        </el-col>
+        <el-col :xs="24" :sm="24" :lg="8">
+          <div class="chart-wrapper">
+            <bar-chart />
+          </div>
+        </el-col>
+      </VueAgile>
     </el-row>
 
     <el-row :gutter="8">
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
+      <el-col
+        :xs="{ span: 24 }"
+        :sm="{ span: 24 }"
+        :md="{ span: 24 }"
+        :lg="{ span: 12 }"
+        :xl="{ span: 12 }"
+        style="padding-right: 8px; margin-bottom: 30px"
+      >
         <transaction-table />
       </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+      <el-col
+        :xs="{ span: 24 }"
+        :sm="{ span: 12 }"
+        :md="{ span: 12 }"
+        :lg="{ span: 6 }"
+        :xl="{ span: 6 }"
+        style="margin-bottom: 30px"
+      >
         <todo-list />
       </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+      <el-col
+        :xs="{ span: 24 }"
+        :sm="{ span: 12 }"
+        :md="{ span: 12 }"
+        :lg="{ span: 6 }"
+        :xl="{ span: 6 }"
+        style="margin-bottom: 30px"
+      >
         <box-card />
       </el-col>
     </el-row>
@@ -50,7 +73,7 @@ import BarChart from './components/BarChart'
 import TransactionTable from './components/TransactionTable'
 import TodoList from './components/TodoList'
 import BoxCard from './components/BoxCard'
-
+import { VueAgile } from 'vue-agile'
 const lineChartData = {
   newVisitis: {
     expectedData: [100, 120, 161, 134, 105, 160, 165],
@@ -81,7 +104,8 @@ export default {
     BarChart,
     TransactionTable,
     TodoList,
-    BoxCard
+    BoxCard,
+    VueAgile
   },
   data() {
     return {
@@ -90,7 +114,7 @@ export default {
   },
   methods: {
     handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type];
+      this.lineChartData = lineChartData[type]
     }
   }
 }
@@ -114,11 +138,36 @@ export default {
     padding: 16px 16px 0;
     margin-bottom: 32px;
   }
+
 }
 
-@media (max-width:1024px) {
+@media (max-width: 1024px) {
   .chart-wrapper {
     padding: 8px;
   }
 }
+</style>
+<style lang="scss" >
+ .agile__nav-button{
+    display: none ;
+  }
+  .agile__dot{
+    &.agile__dot--current{
+      background: #f00;
+    }
+    height: 10px;
+    width: 10px;
+    border-radius:50% ;
+    border: solid 1px #f00;
+    background: transparent;
+    margin: 0 10px;
+button{
+  display: none;
+}
+  }
+  .agile__dots{
+    margin: 0 auto;
+    margin-top: -100px;
+    z-index: 20000;
+  }
 </style>
